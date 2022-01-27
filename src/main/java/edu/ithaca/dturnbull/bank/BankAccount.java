11aab8e3dfec80ca17567f72d21a4f2a96ada496
Annelise Francey <afrancey@ithaca.edu>
2022-01-27 15:21:20 -0500

    fixed isValid code (?) added tests for withdraw

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -44,12 +44,41 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" + 
-        "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
-        Pattern pat = Pattern.compile(emailRegex);
-        if (email == null) {
+        // String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" + 
+        // "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+        // Pattern pat = Pattern.compile(emailRegex);
+
+        // return pat.matcher(email).matches();
+
+        //check for special characters
+        for(int i = 0; i < email.length(); i++){
+            if (email.charAt(i) == '#' || email.charAt(i) == '!' || email.charAt(i) == '&' || email.charAt(i) == '$' || email.charAt(i) == '%' || email.charAt(i) == '^' || email.charAt(i) == '*' || email.charAt(i) == '-' ){
+                return false;
+            }
+        }
+
+        //check if first character is a number
+        if(email.indexOf("0") == 0 || email.indexOf("1") == 0 || email.indexOf("2") == 0 || email.indexOf("3") == 0 || email.indexOf("4") == 0 || email.indexOf("5") == 0 || email.indexOf("6") == 0 || email.indexOf("7") == 0 || email.indexOf("8") == 0 || email.indexOf("9") == 0){
+            return false;
+        } //prob an easier way to do this ^
+
+        //@ or . first
+        if(email.indexOf(".") == 0 || email.indexOf("@") == 0){
+            return false;
+        } 
+
+        //check for @ and .
+        if (email.indexOf('@') == -1 || email.indexOf(".") == -1){
             return false;
         }
-        return pat.matcher(email).matches();
+        
+        //check if empty
+        if (email.isEmpty() == true){
+            return false;
+        }
+        
+        else{
+            return true;
+        }
     }
 }
--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -15,23 +15,33 @@ class BankAccountTest {
 
     @Test
     void withdrawTest() throws InsufficientFundsException{
+        /**
+         * 
+         */
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-        bankAccount.withdraw(100);
 
+        bankAccount.withdraw(0); // test for withdrawing 0
         assertEquals(100, bankAccount.getBalance(), 0.001);
-        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+        bankAccount.withdraw(100); // test for proper withdrawl
+        assertEquals(100, bankAccount.getBalance(), 0.001);
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //withdraw too much - boarder case
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(-100)); // negative amount - boarder case
+       
     }
 
     @Test
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
+
         assertFalse( BankAccount.isEmailValid(""));         // empty string - boarder case
+        assertFalse( BankAccount.isEmailValid("a@bc")); // invalid email - missing .com/etc. - boarder case
+        assertFalse(BankAccount.isEmailValid("@b.com")); //invalid email - missing prefix - boarder case
 
         assertFalse( BankAccount.isEmailValid("a-@b.com"));    // invalid email address with dash in prefix   
         assertFalse( BankAccount.isEmailValid(".a@b.com"));     // invalid email address with period starting the prefix
         assertFalse( BankAccount.isEmailValid("a#a@b.com"));    // invalid email address with hash symbol in the prefix
         assertTrue( BankAccount.isEmailValid("a_b@c.com"));     //valid email - proper use of special char
-        assertTrue( BankAccount.isEmailValid("a@bc")); // invalid email - missing .com/etc. - boarder case
+        
     }
 
 
